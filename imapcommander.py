b8d52d2d4a4a59d845c7c80890c54b3211808b72
Ronald Portier <ronald@portier.eu>
2020-12-11 21:10:19 +0100

    [NEW] Start new imapcommander project.
    This is inspired by offlineimap, but based on python3 and rewritten from
    scratch.

---
+++ imapcommander.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Copyright 2020 - Ronald Portier.
+from imapcommander import ImapCommander
+
+ic = ImapCommander()
+ic.run()
---
+++ imapcommander/imap_commander.py
@@ -0,0 +1,5 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+class ImapCommander:
+    def run(self):
+        """Main command for imap commander."""
+        print("Hello world!")
